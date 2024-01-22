7b7d054b5c6f021691e4acec4e8b16c09f25d002
mca265 <mahimchaudhry4@gmail.com>
2024-01-21 19:56:16 -0800

    Complete styling

--- gpt-clone/src/App.js
+++ gpt-clone/src/App.js
@@ -3,12 +3,32 @@ const App=()=> {
     <div className="app">
       <section className="side-bar">
         <button>+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li></li>
+        </ul>
         <nav>
           <p>Made by Mahim</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>MahimGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+
+          </div>
+          <p className="info">
+            Mahim GPT. Free Research Preview.
+            This project was made with the help of 
+            React and Node.js.
+          </p>
+
+        </div>
+      </section>
     </div>
   );
 }
