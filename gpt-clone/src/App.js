826e62e203907f800e971eb079e59988a4787937
mca265 <mahimchaudhry4@gmail.com>
2024-01-21 19:11:04 -0800

    Create basic layout

--- gpt-clone/src/App.js
+++ gpt-clone/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Mahim</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
   );
 }
