31503165d0945befa9d4face8ae02868b6f56c49
mca265 <mahimchaudhry4@gmail.com>
2024-01-22 20:08:02 -0800

    Implement chat message retrieval

--- gpt-clone/server.js
+++ gpt-clone/server.js
@@ -7,4 +7,27 @@ app.use(express.json())
 app.use(cors())
 
 const API_KEY = process.env.API_KEY
-app.listen(PORT,() => console.log('Your server is running on PORT '+ PORT))
+
+app.post('/completions', async(req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: "how are you?" }],
+            max_tokens: 10,
+        })
+    };
+    try {
+        const response= await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
--- gpt-clone/src/App.js
+++ gpt-clone/src/App.js
@@ -1,4 +1,24 @@
 const App=()=> {
+
+const getMessages = async()=>{
+ const options = {
+  method: "POST",
+  body : JSON.stringify({
+    message: "hello how are you?"
+  }),
+  headers: {
+    "Content-Type": "application/json"
+  }
+ }
+  try{
+    const response = await fetch('http://localhost:8000/completions', options)
+    const data = await response.json()
+    console.log(data)
+  } catch(error){
+    console.error(error);
+  }
+}
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -18,7 +38,7 @@ const App=()=> {
         <div className="bottom-section">
           <div className="input-container">
             <input/>
-            <div id="submit">➢</div>
+            <div id="submit" onClick={getMessages}>➢</div>
 
           </div>
           <p className="info">
